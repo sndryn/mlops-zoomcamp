f6895188276f426724c247968d266e55107dae4a
steffiindrayani <steffiindrayani16@gmail.com>
2025-07-05 18:17:11 +0700

    cd-deploy

--- 06-best-practices/lesson/src/infrastructure/main.tf
+++ 06-best-practices/lesson/src/infrastructure/main.tf
@@ -61,3 +61,19 @@ module "lambda_function" {
     source_stream_name      = "${var.source_stream_name}-${var.project_id}"
     source_stream_arn       =  module.source_kinesis_stream.stream_arn
 }
+
+output "lambda_function" {
+    value   = "${var.lambda_function_name}_${var.project_id}"
+}
+
+output "model_bucket_name" {
+    value   = module.s3_bucket.name
+}
+
+output "prediction_stream_name" {
+    value   = "${var.output_stream_name}-${var.project_id}"
+}
+
+output "ecr_repo_name" {
+    value   = "${var.ecr_repo_name}_${var.project_id}"
+}
